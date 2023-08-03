59a799aef14566e311eba061f76c1b9f8513d73c
chen323-ux <134175525+chen323-ux@users.noreply.github.com>
2023-08-03 12:51:26 -0700

    Update nmain.py

--- nmain.py
+++ nmain.py
@@ -5,7 +5,7 @@ import os
 import streamlit as st
 import pandas as pd
 import tempfile
-import numpy as np
+import re
 
 def main():
 
@@ -49,12 +49,45 @@ def main():
             st.error("API Key error.")
             st.error(str(e))
 
+        if "can you show me where it is" in user_question:
+            st.text("Yes, of couse! Give me 5 seconds at most")
+            def extract_coordinates(input_text):
+                try:
+                    # Use regular expression to find latitude and longitude in the input text
+                    # First, try to find the format "34.149917 latitude and -118.616783 longitude."
+                    pattern1 = r'(-?\d+\.\d+)\s*latitude\s*and\s*(-?\d+\.\d+)\s*longitude\.'
+                    match1 = re.search(pattern1, input_text)
 
-    df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+                    # If the first format is not found, try to find the format "34.10155, -118.336883."
+                    if not match1:
+                        pattern2 = r'(-?\d+\.\d+),\s*(-?\d+\.\d+)'
+                        match2 = re.search(pattern2, input_text)
+                        if not match2:
+                            raise ValueError
+                        lat, lon = map(float, match2.groups())
+                    else:
+                        lat, lon = map(float, match1.groups())
 
-    st.map(df)
+                    return lat, lon
+
+                except Exception as e:
+                    st.error("Error extracting coordinates. Please enter a valid input.")
+                    return None, None
+
+            # Get user input
+            user_input = agent.run(user_question)
+
+            # Extract coordinates from the user input
+            latitude, longitude = extract_coordinates(user_input)
+
+            # Display the map with the extracted coordinates
+            if latitude is not None and longitude is not None:
+                df = pd.DataFrame({
+                    'lat': [latitude],
+                    'lon': [longitude]
+                })
+
+                st.map(df)
 
 
 
@@ -62,5 +95,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 
